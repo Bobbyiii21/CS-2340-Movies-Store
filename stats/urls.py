a3d4bae605cef5d7b54faa9e60099fac71adad62
Ellie Fiftal <gfiftal@gmail.com>
2026-03-09 05:56:23 -0400

    Funcitonal viewing of top movie purchaser and their orders

--- stats/urls.py
+++ stats/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.index, name='stats.index'),
     path('users/', views.users, name='stats.users'),
     path('movies/', views.movies, name='stats.movies'),
+    path('users/<int:id>/see_orders/', views.see_orders, name='stats.see_orders'),
 ]
